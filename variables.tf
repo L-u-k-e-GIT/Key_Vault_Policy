5d4ad81813bb6cab39f1f33426e865f1973826a9
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-09-01 16:08:32 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+
+
+variable "MD_KV_ID" {
+  description = "Key Vault ID"
+  type        = string
+  default     = ""
+
+}
+
+variable "MD_VM_IDENTITY" {
+  description = "VM Identity to enable access"
+  type        = string
+  default     = ""
+
+}
+
+variable "MD_ARM_TENANT_ID" {
+  description = "Tenand Id"
+  type        = string
+  default     = ""
+
+}
+
