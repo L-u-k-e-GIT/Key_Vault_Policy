6c98367e3995dd9cb772065f88a2ba9263e14d44
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-09-01 16:10:12 +0200

    Update key_vault_policy.tf

--- key_vault_policy.tf
+++ key_vault_policy.tf
@@ -1,7 +1,7 @@
 resource "azurerm_key_vault_access_policy" "vm_identity_policy" {
   key_vault_id = var.MD_KEY_VAULT
   tenant_id    = var.MD_ARM_TENANT_ID
-  object_id    = var.MD_VM_IDENTIY
+  object_id    = var.MD_VM_IDENTITY
 
    secret_permissions = [
     "Get",  "Delete", "Set", "Purge", "Recover" , "List", "Backup"
